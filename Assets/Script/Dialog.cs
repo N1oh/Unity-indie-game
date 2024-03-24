3e9dc1311f9dbe51e4e16cb9574877a3a999845e
Nohs <153786970+N1oh@users.noreply.github.com>
2024-03-24 20:32:19 +0900

    fix
    수정

--- Assets/Script/Dialog.cs
+++ Assets/Script/Dialog.cs
@@ -18,8 +18,11 @@ public class Dialog : MonoBehaviour
     [SerializeField] private SpriteRenderer sprite_StandingCG; // 캐릭터 이미지를 표시하는 SpriteRenderer
     [SerializeField] private SpriteRenderer sprite_DialogueBox; // 대화 상자를 표시하는 SpriteRenderer
     [SerializeField] private Text txt_Dialogue; // 대화 텍스트를 표시하는 Text 컴포넌트
+    [SerializeField] private Vector2 leftPosition; // 왼쪽에 표시할 좌표
+    [SerializeField] private Vector2 rightPosition;// 오른쪽에 표시할 좌표
 
     [SerializeField] private Dialogue[] dialogue; // 대화 정보를 담고 있는 배열
+
 
     private bool isDialogue = false; // 대화 중인지 여부를 나타내는 플래그
     private int count = 0; // 현재 대화 인덱스를 저장하는 변수
@@ -50,11 +53,11 @@ public class Dialog : MonoBehaviour
         // 이미지 위치를 설정
         if (dialogue[count].isLeft)
         {
-            sprite_StandingCG.transform.localPosition = new Vector2(-322f, 22f); // 왼쪽에 표시
+            sprite_StandingCG.transform.localPosition = leftPosition; // 왼쪽에 표시
         }
         else
         {
-            sprite_StandingCG.transform.localPosition = new Vector2(266f, 22f); // 오른쪽에 표시
+            sprite_StandingCG.transform.localPosition = rightPosition; // 오른쪽에 표시
         }
 
         count++; // 다음 대화로 이동
@@ -62,23 +65,28 @@ public class Dialog : MonoBehaviour
 
     void Update()
     {
-        // 대화 중일 때
-        if (isDialogue)
+        // 게임이 퍼즈 상태인지 확인
+        if (Time.timeScale != 0)
         {
-            // 마우스 왼쪽 버튼을 클릭했을 때
-            if (Input.GetMouseButtonDown(0))
+            // 대화 중일 때
+            if (isDialogue)
             {
-                // 대화가 남아있을 경우
-                if (count < dialogue.Length)
+                // 마우스 왼쪽 버튼을 클릭했을 때
+                if (Input.GetMouseButtonDown(0))
                 {
-                    NextDialogue(); // 다음 대화로 이동
-                }
-                // 대화가 끝났을 경우
-                else
-                {
-                    OnOff(false); // 모든 대화가 끝나면 대화 종료
+                    // 대화가 남아있을 경우
+                    if (count < dialogue.Length)
+                    {
+                        NextDialogue(); // 다음 대화로 이동
+                    }
+                    // 대화가 끝났을 경우
+                    else
+                    {
+                        OnOff(false); // 모든 대화가 끝나면 대화 종료
+                    }
                 }
             }
         }
     }
 }
+
